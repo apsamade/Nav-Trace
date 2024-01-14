e1eda2c3651c31ced3ff71a689d18cbb74982d42
apsamade <bouderga.abdessamade@gmail.com>
2024-01-14 22:44:03 +0100

    maj

--- controller/admin/product/deleteProduct.js
+++ controller/admin/product/deleteProduct.js
@@ -11,6 +11,21 @@ exports.getDeleteProduct = async (req, res, next)=>{
     }
 }
 
-exports.postDeleteProduct = (req, res, next)=>{
+exports.postDeleteProduct = async (req, res, next)=>{
+    const user = req.session.user
+    const productId = req.params.id
+    try {
+        if(user && user.admin){
+            await Product.findByIdAndDelete(productId)
+            console.log('Produit détruit avec succès !')
+            res.render('admin/product/deleteProduct', { user, message: 'Produit supprimer avec succès !' })
+        }else{
+            res.redirect('/connexion')
+        }        
+    } catch (error) {
+        console.log(error)
+        const produit = await Product.findById(productId)
+        res.render('admin/product/deleteProduct', { user, produit })
+    }
 
 }
