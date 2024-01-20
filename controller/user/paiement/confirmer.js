a41a8f2e4943f64e17c344b26733e39ccaf0833a
apsamade <bouderga.abdessamade@gmail.com>
2024-01-20 19:29:01 +0100

    maj

--- controller/user/paiement/confirmer.js
+++ controller/user/paiement/confirmer.js
@@ -3,14 +3,17 @@ const Panier = require('../../../models/panier')
 exports.getConfirmation = async (req, res, next)=>{
     const user = req.session.user
     const panier = req.session.panier
-    const thisPanier = await Panier.findById(panier._id)
-    if(user && thisPanier.payer){
-        res.render('paiement/confirmer', { user, panier })
-    }else if(user && !thisPanier.payer){
-        res.render('paiement/confirmer', { user, panier, erreur : "Panier non payé !" })
-    }else{
-        res.redirect('/connexion')
+    if(panier){
+        const thisPanier = await Panier.findById(panier._id)
+        if(user && thisPanier.payer){
+            res.render('paiement/confirmer', { user, panier })
+        }else if(user && !thisPanier.payer){
+            res.render('paiement/confirmer', { user, panier, erreur : "Panier non payé !" })
+        }else{
+            res.redirect('/connexion')
+        }        
     }
+
 }
 
 exports.postConfirmation = (req, res, next)=>{
