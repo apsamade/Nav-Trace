63fc38e75ec5be584cad2f8f94ae26ab53d364b0
apsamade <bouderga.abdessamade@gmail.com>
2024-01-21 18:10:36 +0100

    maj

--- controller/user/paiement/confirmer.js
+++ controller/user/paiement/confirmer.js
@@ -1,10 +1,12 @@
 const Panier = require('../../../models/panier')
+const stripe = require('stripe')
 
 exports.getConfirmation = async (req, res, next)=>{
     const user = req.session.user
     const panier = req.session.panier
     const thisPanier = await Panier.findById(panier._id)
-    const session = await stripe.checkout.sessions.retrieve(panier.paiement_stripe_id);
+    const sessionId = req.query.session_id
+    const session = await stripe.checkout.sessions.retrieve(sessionId);
     console.log(session)
     if(panier){
         if(user && session.payment_status === 'paid' && session.consent.terms_of_service == 'accepted'){
