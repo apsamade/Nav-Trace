06b3652aad2030850688d8fb5fccd4ca673d16b9
apsamade <bouderga.abdessamade@gmail.com>
2024-01-21 23:09:53 +0100

    maj

--- controller/user/paiement/confirmer.js
+++ controller/user/paiement/confirmer.js
@@ -1,26 +1,24 @@
 const Panier = require('../../../models/panier')
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
-exports.getConfirmation = async (req, res, next)=>{
+exports.getConfirmation = async (req, res, next) => {
     const user = req.session.user
-    const panier = req.session.panier
     const sessionId = req.query.session_id
     const session = await stripe.checkout.sessions.retrieve(sessionId);
     console.log(session)
-    if(panier){
-        if(user && session.payment_status === 'paid' && session.consent.terms_of_service == 'accepted'){
-            delete req.session.panier
-            res.render('paiement/confirmer', { user, panier })
-        }else if(user && session.payment_status != 'paid'){
-            res.render('paiement/confirmer', { user, panier, erreur : "Panier non payé !" })
-        }else{
-            res.redirect('/connexion')
-        }        
+    if (user && session.payment_status === 'paid' && session.consent.terms_of_service == 'accepted') {
+        delete req.session.panier
+        res.render('paiement/confirmer', { user })
+    } else if (user && session.payment_status != 'paid') {
+        const panier = req.session.panier
+        res.render('paiement/confirmer', { user, panier, erreur: "Panier non payé !" })
+    } else {
+        res.redirect('/connexion')
     }
 
 }
 
-exports.postConfirmation = (req, res, next)=>{
+exports.postConfirmation = (req, res, next) => {
 
 
 }
