ec5eb16ce2246ffb2f33730d7f0ba7e3d1cc782f
apsamade <bouderga.abdessamade@gmail.com>
2024-02-06 19:49:54 +0100

    maj

--- controller/user/paiement/paiement.js
+++ controller/user/paiement/paiement.js
@@ -90,10 +90,16 @@ exports.postPayement = async (req, res, next) => {
                 consent_collection: {
                     terms_of_service: 'required',
                 },
+                shipping_address_collection: {
+                    allowed_countries: ['FR'],
+                },
                 custom_text: {
                     terms_of_service_acceptance: {
                         message: 'Je suis d\'accord avec les [Conditions d\'utilisation](https://nav-trace.onrender.com/politique)',
-                    }
+                    },
+                    shipping_address: {
+                        message: 'La livraison en 2 jours n\'est actuellement pas assurée pour les boîtes postales.',
+                    },
                 },
                 phone_number_collection: {
                     enabled: true,
